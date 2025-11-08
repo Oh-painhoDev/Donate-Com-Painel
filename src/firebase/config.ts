bb06f6bbf7adf4443c2185637573718532d41fa8
Marcelo Douglas <douglas20345@gmail.com>
2025-11-08 17:54:50 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3315887496-3dd78",
+  "appId": "1:758640222493:web:df29b5069832dc17a5c1f4",
+  "apiKey": "AIzaSyDd3YhleDUGuS-h9NB-HZECCyDvclmA-ws",
+  "authDomain": "studio-3315887496-3dd78.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "758640222493"
+};
